f8fb3c46d9a14fc52777d3899c9beaacf5e7717b
helenzhangg <h.zhang2021@gmail.com>
2016-05-13 18:44:35 -0400

    added files for multiplier

--- multiplier/ViewController.swift
+++ multiplier/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var currentMultiple = 0
+    var maxTaps = 12
+    var currentTaps = 0
+    var multipleInput = 0
+    
+    //Outlets
+
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var multiple: UITextField!
+    
+    @IBOutlet weak var initialInstruc: UILabel!
+    @IBOutlet weak var addition: UILabel!
+    @IBOutlet weak var add: UIButton!
+
+
+    //Starting the game
+    
+    @IBAction func onPlay(sender: UIButton!) {
+        if multiple.text != nil && multiple.text != "" {
+            logoImg.hidden = true
+            playButton.hidden = true
+            multiple.hidden = true
+            addition.hidden = true
+            
+            add.hidden = false
+            initialInstruc.hidden = false
+            
+            currentMultiple = 0
+            currentTaps = 0
+            multipleInput = Int(multiple.text!)!
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateMultipleLabel(oldSum: Int, mul: Int, newSum: Int) {
+        addition.text = " \(oldSum) + \(mul) = \(newSum)"
     }
-
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    @IBAction func addTap(sender: AnyObject!) {
+        if isGameOver() {
+            restart()
+        }
+        currentTaps++
+        let newSum = currentMultiple + multipleInput
+        updateMultipleLabel(currentMultiple, mul: multipleInput, newSum: newSum)
+        
+        if currentTaps == 1{
+            initialInstruc.hidden = true
+            addition.hidden = false
+    }
+        if currentTaps >= 1{
+            currentMultiple += multipleInput
+        }
+    }
+    
+    @IBAction func restart() {
+        
+        
+        currentTaps = -1
+        currentMultiple = 0
+        
+        logoImg.hidden = false
+        playButton.hidden = false
+        multiple.hidden = false
+        multiple.text = nil
+        
+        addition.hidden = true
+        add.hidden = true
+        
+    }
 
 }
-
